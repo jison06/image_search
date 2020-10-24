99951bccf6cf10ea4c7d101fbfc08faa0451dda6
jison06 <ison.84@buckeyemail.osu.edu>
2020-10-24 18:06:55 -0400

    Added search field

---
+++ src/components/SearchBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { searchTerm: '' };
+
+  onInputChange(e) {
+    console.log(e.target.value);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" onChange={this.onInputChange}></input>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
