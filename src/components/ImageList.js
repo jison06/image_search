36c6c7150fe86cd8c65edddc2e6507e78012dc05
jison06 <ison.84@buckeyemail.osu.edu>
2020-12-21 17:22:12 -0500

    Created image card component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,10 @@
 import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
-  const images = props.images.map(({ description, id, urls }) => (
-    <img key={id} src={urls.small} alt={description} />
+  const images = props.images.map(image => (
+    <ImageCard key={image.id} image={image} />
   ));
   return <div className="image-list">{images}</div>;
 };
